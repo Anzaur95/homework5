2c1d4c8e96eb7fa393971f820ce301c3ecf7c02f
Anzaur95 <anzaur-1992@mail.ru>
2024-07-27 20:06:48 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = 1, True, 'cat'
+print( immutable_var)
+#  immutable_var[0] = 2 - попытался изменить элементы кортежа, но выдает ошибку. Котрежи нельзя изменить, так нам объяснили на онлайн уроке.
+mutable_list = ['привет', 'меня зовут', 'Анзаур']
+mutable_list [2] = 'Anzaur'
+print(mutable_list)
